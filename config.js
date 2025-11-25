e4d5676b40d14a81c881f1d6d947f205ba42ad45
ronaldomazive915-source <ronaldomazive915@gmail.com>
2025-11-25 16:41:31 +0200

    Update bot configuration details

--- config.js
+++ config.js
@@ -13,7 +13,7 @@ module.exports = {
     AUTO_STATUS_SEEN: convertToBool(process.env.AUTO_STATUS_SEEN, true),
     AUTO_STATUS_REPLY: convertToBool(process.env.AUTO_STATUS_REPLY, false),
     AUTO_STATUS_REACT: convertToBool(process.env.AUTO_STATUS_REACT, true),
-    AUTO_STATUS_MSG: process.env.AUTO_STATUS_MSG || "*YOUR STATUS HAS BEEN SEEN BY BLUE-XMD* 😊",
+    AUTO_STATUS_MSG: process.env.AUTO_STATUS_MSG || "*YOUR STATUS HAS BEEN SEEN BY RONY-SKIES* 😊",
 
     // === WELCOME / EVENTS ===
     WELCOME: convertToBool(process.env.WELCOME, true),
@@ -47,36 +47,36 @@ module.exports = {
     AUTO_TYPING: convertToBool(process.env.AUTO_TYPING, false),
 
     // === OWNER & BOT DETAILS ===
-    OWNER_NUMBER: process.env.OWNER_NUMBER || "2349041772077",
-    OWNER_NAME: process.env.OWNER_NAME || "Mr Emerald",
-    BOT_NAME: process.env.BOT_NAME || "Blue-xmd",
+    OWNER_NUMBER: process.env.OWNER_NUMBER || "27696397895",
+    OWNER_NAME: process.env.OWNER_NAME || "Mr RONY",
+    BOT_NAME: process.env.BOT_NAME || "RONY-SKIES",
     PREFIX: process.env.PREFIX || ".",
-    STICKER_NAME: process.env.STICKER_NAME || "Blue-xmd",
-    DESCRIPTION: process.env.DESCRIPTION || "*© ᴘᴏᴡᴇʀᴇᴅ ʙʏ BLUE-XMD*",
-    DEV: process.env.DEV || "2349041772077",
+    STICKER_NAME: process.env.STICKER_NAME || "RONY-SKIES",
+    DESCRIPTION: process.env.DESCRIPTION || "*© ᴘᴏᴡᴇʀᴇᴅ ʙʏ RONY-SKIES*",
+    DEV: process.env.DEV || "27696397895",
 
     // === MESSAGES & MEDIA ===
-    MENU_IMAGE_URL: process.env.MENU_IMAGE_URL || "https://files.catbox.moe/lthtjq.jpeg",
-    ALIVE_IMG: process.env.ALIVE_IMG || "https://files.catbox.moe/gcojxb.jpg",
-    LIVE_MSG: process.env.LIVE_MSG || "🚀 *BLUE-XMD  is Alive!* 🚀",
+    MENU_IMAGE_URL: process.env.MENU_IMAGE_URL || "https://files.catbox.moe/sk5pst.jpg",
+    ALIVE_IMG: process.env.ALIVE_IMG || "https://files.catbox.moe/sk5pst.jpg",
+    LIVE_MSG: process.env.LIVE_MSG || "🚀 *RONY-SKIES is Alive!* 🚀",
 
     // === ANTI DELETE / VIEW ONCE ===
     ANTI_VV: convertToBool(process.env.ANTI_VV, true),
     ANTI_DEL_PATH: process.env.ANTI_DEL_PATH || "log",
 
     // === LINKS / SOCIAL ===
-    BOT_CHANNEL: process.env.BOT_CHANNEL || "https://whatsapp.com/channel/0029Vb6OLyzEAKWFsk7REX3r",
-    REPO_LINK: process.env.REPO_LINK || "https://github.com/emeraldlevels/BLUE-XMD",
-    YOUTUBE: process.env.YOUTUBE || "https://www.youtube.com/@EmeraldLevels",
-    TIKTOK: process.env.TIKTOK || "https://tiktok.com/@emeralds.levels",
-    INSTAGRAM: process.env.INSTAGRAM || "",// fill in if available
+    BOT_CHANNEL: process.env.BOT_CHANNEL || "https://whatsapp.com/channel/0029VbBnRol1XquTPCwUsk15",
+    REPO_LINK: process.env.REPO_LINK || "https://github.com/ronaldomazive915-source",
+    YOUTUBE: process.env.YOUTUBE || "https://www.youtube.com/@ronyskies1",
+    TIKTOK: process.env.TIKTOK || "https://tiktok.com/@ronaldomazive1905",
+    INSTAGRAM: process.env.INSTAGRAM || "@ronaldo_skies",// fill in if available
     TWITTER: process.env.TWITTER || "", // fill in if available
-    DISCORD: process.env.DISCORD || "https://discord.gg/f3RNWAh2",
+    DISCORD: process.env.DISCORD || "",
     WEBSITE: process.env.WEBSITE || "",// fill in if available
-    COMMUNITY: process.env.COMMUNITY || "https://chat.whatsapp.com/EdtpVr9VqbPJwWYdH92vXI?mode=ems_copy_c",
+    COMMUNITY: process.env.COMMUNITY || "https://chat.whatsapp.com/BL0erai5W6O1dKGMczM7vG?mode=wwt",
 
     // === BANK DETAILS (optional / for placeholders) ===
     BANK_NAME: process.env.BANK_NAME || "access bank",// replace with your bank name
-    ACCOUNT_NUMBER: process.env.ACCOUNT_NUMBER || "1486604137", // replace with your account number
-    ACCOUNT_NAME: process.env.ACCOUNT_NAME || "fransisca kelechi Okonkwo" // replace with your account name
+    ACCOUNT_NUMBER: process.env.ACCOUNT_NUMBER || "4137", // replace with your account number
+    ACCOUNT_NAME: process.env.ACCOUNT_NAME || "Ronaldo Ginilio Mazive" // replace with your account name
 };
